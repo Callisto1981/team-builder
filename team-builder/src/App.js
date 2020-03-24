77a34c21b7731e04eec3abfc69c5b3ee7fade415
Wayne <wabbruscatoorr@gmail.com>
2020-03-24 19:09:24 -0400

    added member.js file

---
+++ team-builder/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Form from "./form";
+import Members from "./members";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
